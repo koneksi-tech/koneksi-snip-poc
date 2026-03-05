5815b6cd531a7bc39c943c60f1775460c32ec0a3
Franz <franzegos9@gmail.com>
2026-03-05 14:04:25 +0800

    fix: build error

--- src/components/UploadHistory.tsx
+++ src/components/UploadHistory.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
-  getUploadHistory,
   removeFromUploadHistory,
   clearUploadHistory,
   type StoredUpload,
